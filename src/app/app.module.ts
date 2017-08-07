6f16fa076f97c3f17b9def301bb231b64112ff58
mario <panuwat_koktomchai@hotmail.com>
2017-08-07 14:54:04 +0700

    'update'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  ArrayData={
+    id:'',
+    name:'',
+    description:''
+  };
+  constructor(public navCtrl: NavController,private BCScanner: BarcodeScanner) {
 
   }
-
+  opencamera(){
+    this.BCScanner.scan().then((Mydata)=>{
+      //suscesse
+      this.ArrayData.id = Mydata.text;
+    },(Error)=>{
+      //error
+      alert(Error);
+    });
+  }
+ 
 }
